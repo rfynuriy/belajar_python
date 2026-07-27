3860cb2cb645947c476b4b40938f3eb0aa44f4cf
rfynuriy <nurrohimraffi0@gmail.com>
2026-07-27 14:56:36 +0700

    penyelesaian

--- mini_projek_minggu3.py
+++ mini_projek_minggu3.py
@@ -5,15 +5,17 @@ class produk():
         self.harga = harga
         self.stok = stok
     def total_nilai_barang(self):
-        return f"{(self.stok * self.harga): ,}"
-    def banyaknya_barang(self):
-        return f"jumlah barangnya ada -{len(self.barang)}"
+        return f"total harganilai barangnya = {(self.stok * self.harga): ,}Rp"
     def info(self):
-        return f" barang = {self.nama}, harga = {self.harga}, stok = {self.stok}"
+            return f" barang = {self.nama}, harga = {self.harga}, stok = {self.stok}"
+
 produk1 = produk("shampo", 40000, 24)
 produk2 = produk("sabun", 5000, 30)
-
+daftar_produk = [produk1, produk2]
 
 print(produk1.total_nilai_barang())
+print(produk2.total_nilai_barang())
+print(produk1.info(), produk2.info())
+
 
 
