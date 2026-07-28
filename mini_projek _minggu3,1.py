4335f617760dfe23d77afcd885544f2ee5131eac
rfynuriy <nurrohimraffi0@gmail.com>
2026-07-28 11:56:20 +0700

    memnyelesaikan file mini projek minggu3.1

---
+++ mini_projek _minggu3,1.py
@@ -0,0 +1,27 @@
+"""
+try:
+    angka1 = int(input("hello masukan angka pertama: "))
+    angka2 = int(input("masukkan angka kedua: "))
+    print(f"hasil baginya adalah: {angka1 / angka2:.2f}")
+except ValueError as e:
+    print(f"input yang diberikan tidak falid! {e}: ")
+except ZeroDivisionError:
+    print("angka tak boleh 0")
+#"""
+
+#"""
+
+print (hasil = "")
+try:
+    angka1 = int(input("hello masukan angka pertama: "))
+    angka2 = int(input("masukkan angka kedua: "))
+    hasil= f"{angka1 / angka2:.2f}"
+except ValueError as e:
+    print(f"input yang diberikan tidak falid! {e}: ",)
+except ZeroDivisionError:
+    print("angka tak boleh 0")
+else:
+    print(f"input yang dimasukan falid {angka1} / {angka2}")
+finally: # ini akan tetap maksa keluar mau hasil reor atau tidak dia gapeduli
+    print(f"hasil = {hasil}")
+#"""
