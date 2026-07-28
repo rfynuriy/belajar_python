8de4fa04913780f1839479b0aa41a48c560f0a7f
rfynuriy <nurrohimraffi0@gmail.com>
2026-07-28 19:37:51 +0700

    penyelesaian modifikasi file

--- modifikasi_file/minggu_3_file.py
+++ modifikasi_file/minggu_3_file.py
@@ -1,9 +1,12 @@
+
+"""
+
 f = open("apapun.txt", "w")
 f.write("hallo ini adalah baris pertama\n")
 f.write("ini merupakan baris kedua\n")
 f.close()
 
-b = open("apapun.txt", "r") # buka ( "<file>" "<perintah>" "<perintah>")
+b = open("apapun.txt", "r") # buka ( "<file>" "<perintah>")
 isi = b.read() # fariable = perintah baca
 print(isi) # esekusi fariable
 b.close() # tutup
@@ -12,6 +15,8 @@ with open("apapun.txt", "r") as fariable: # buka lalu tutup
     cek = fariable.read() # pembuatan fariable = perintah baca
     print(cek) # eksekusi fariable
 
+#"""
+
 """
 PERINTAH
 
@@ -19,5 +24,17 @@ PERINTAH
 "w" - Write  - tulis ke file (kalau file udah ada, isi lama akan HILANG/ketimpa; kalau belum ada, file baru dibuat)
 "a" - Append - tambah data di akhir file (isi lama tetap ada, data baru ditambahin di bawahnya)
 "x" - Create - bikin file baru (error kalau file udah ada duluan)
-
+tambahan
+"r+" - Baca dan tulis (file harus udah ada)
+"w+" - Tulis dan baca (isi lama tetap ke-hapus, kayak mode "w" biasa)
+"a+" - Tambah (append) dan baca (isi lama tetap ada)
 """
+
+
+isi_manual = str(input("mengatik: "))
+with open("apapun.txt", "a") as fung:
+    fung.write(f"{isi_manual}\n")
+
+with open("apapun.txt", "r") as bca:
+    baca = bca.read()
+    print(baca)
