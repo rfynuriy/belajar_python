a4274f941ab42b42acf76dd9eb49c4f2e81373bf
rfynuriy <nurrohimraffi0@gmail.com>
2026-07-28 17:31:59 +0700

    membuat mini projek membuka projek

---
+++ modifikasi_file/minggu_3_file.py
@@ -0,0 +1,23 @@
+f = open("apapun.txt", "w")
+f.write("hallo ini adalah baris pertama\n")
+f.write("ini merupakan baris kedua\n")
+f.close()
+
+b = open("apapun.txt", "r") # buka ( "<file>" "<perintah>" "<perintah>")
+isi = b.read() # fariable = perintah baca
+print(isi) # esekusi fariable
+b.close() # tutup
+
+with open("apapun.txt", "r") as fariable: # buka lalu tutup
+    cek = fariable.read() # pembuatan fariable = perintah baca
+    print(cek) # eksekusi fariable
+
+"""
+PERINTAH
+
+"r" - Read   - baca file (file harus udah ada, kalau gak ada, error)
+"w" - Write  - tulis ke file (kalau file udah ada, isi lama akan HILANG/ketimpa; kalau belum ada, file baru dibuat)
+"a" - Append - tambah data di akhir file (isi lama tetap ada, data baru ditambahin di bawahnya)
+"x" - Create - bikin file baru (error kalau file udah ada duluan)
+
+"""
