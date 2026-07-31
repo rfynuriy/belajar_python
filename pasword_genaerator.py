2388a1157501090319d98e06b99984d62d4af7cd
rfynuriy <nurrohimraffi0@gmail.com>
2026-07-31 16:29:32 +0700

    buat pw geneerator serta kerangkanya

---
+++ pasword_genaerator.py
@@ -0,0 +1,20 @@
+import random
+import string
+import datetime
+
+#   pw generator   #
+
+class gensandi:
+    def __init__(self):
+        self.aktivitas_pembuatan_sandi = []
+
+    def generate (self, panjang, simbol):
+        pass
+
+    def catat_aktivitas(self):
+        pass
+
+    def baca_riwyat(self):
+        pass
+
+
