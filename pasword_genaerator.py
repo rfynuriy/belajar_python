c17bf9a32f60b80191f594951d3b39995bd53080
rfynuriy <nurrohimraffi0@gmail.com>
2026-08-01 14:38:13 +0700

    buat fitur pw generatur & catatan otomatis

--- pasword_genaerator.py
+++ pasword_genaerator.py
@@ -1,20 +1,51 @@
 import random
 import string
-import datetime
+from datetime import datetime
 
 #   pw generator   #
 
-class gensandi:
+class GenSandi:
     def __init__(self):
         self.aktivitas_pembuatan_sandi = []
 
     def generate (self, panjang, simbol):
-        pass
+        defauld = string.ascii_uppercase + string.ascii_lowercase + string.digits
+        if simbol == True:
+            defauld = defauld + string.punctuation
+        karakter_terpilih = []
+        for i in range(panjang):
+            satu_karakter = random.choice(defauld)
+            karakter_terpilih.append(satu_karakter)
 
-    def catat_aktivitas(self):
-        pass
+        pw = "".join(karakter_terpilih)
+        return pw
+
+
+    def catat_aktivitas(self, p, s):
+        sekarang = datetime.now()
+        waktu_lenkap = sekarang.strftime("%d-%m-%Y %H:%M:%S")
+        catatan = f"waktu pembuatan = \" {waktu_lenkap} \" | panjang karakter = {p} | simbol = {s}"
+        self.aktivitas_pembuatan_sandi.append(catatan)
+        with open("catatan_pw_gen.txt", "a") as simpan:
+            simpan.write(f"{catatan}, \n")
+
+
 
     def baca_riwyat(self):
         pass
 
 
+
+
+random_sandi = GenSandi()
+while True:
+    try:
+        panjang = int(input("masukkan panjang sandi: "))
+        simbol = input("ketik \"y\" jika ingin menggunakan simbol: ").lower() == "y"
+        if simbol == True:
+            break
+        else:
+            break
+    except ValueError:
+        print("tolong masukan angka")
+print(random_sandi.generate(panjang, simbol))
